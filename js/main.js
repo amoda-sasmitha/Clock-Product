f8211c5d8039589865af950d87a50271c37e906e
lucidex <padulaguruge@gmail.com>
2020-05-30 22:34:23 +0530

    added comments

--- js/main.js
+++ js/main.js
@@ -61,7 +61,7 @@ function submitform(event){
         Username : "lucidexclientsangeeth@gmail.com",
         Port : "587",
         Password : "FpygaT1A4Z8xECb7",
-        To : 'amoda29@gmail.com',
+        To : 'padulaguruge@gmail.com',
         From : "lucidexclientsangeeth@gmail.com",
         Subject : "Form Data Submit",
         Body : msg
